8ae85e347ccd234134ecd8e8ae7a7c7676d6d9cd
Morrese Green Jr <greenmo57@gmail.com>
2021-09-01 09:15:24 -0400

    unfinished project

---
+++ python_project1.py
@@ -0,0 +1,16 @@
+class Restaurant:
+    def __init__(self, menu = ["burgers", "hot_dogs","wings","fries"], customer = {("name":"Mo")}, order = 0):
+        self.menu = menu
+        self.customer = customer
+        self.order = order
+        
+def menu_items(self, menu_name, menu):
+    self.items.append(burgers, hot_dogs, wings,fries)
+
+def add_menu(self, )
+
+def remove_menu(self,)
+
+def customer_identity(self, customer):
+
+def order_items(self,  )
